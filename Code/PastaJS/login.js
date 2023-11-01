e6c0bdfd1c1b6594f4caa2a0f0a3107094e48bc2
melbo <80221337+melnxb@users.noreply.github.com>
2023-11-01 20:48:28 -0300

    login+Admin=1

---
+++ Code/PastaJS/login.js
@@ -0,0 +1,34 @@
+function login(){
+    var form = document.getElementById('login');
+    var dados = new FormData(form);
+    
+
+    fetch("../PastaPHP/login.php", {
+        method: "POST",
+        body: dados
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.error) {
+            alert(data.error);
+        } else if (data.redirect) {
+            window.location.href = data.redirect;
+        }
+    });
+    
+    var user = form.elements['username'].value;
+    var senha = form.elements['password'].value;
+    if(user.length=="" && senha.length==""){
+        alert("Preencha os campos para fazer o login!");
+        return false;
+    }
+    else if(user.length==""){
+        alert(" Preencha p nome de usuario!");
+        return false;
+    }
+    else if(senha.length==""){
+        alert(" Informe a senha!");
+        return false;
+    }
+    return false;
+}
